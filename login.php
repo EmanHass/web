49bfd6f8568fba4f41c875aec0f0c0ede279a847
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2021-04-23 17:47:27 +0300

    Update login.php

--- login.php
+++ login.php
@@ -70,42 +70,82 @@
 </body>
 </html>
 <?php
-	
-	
-	// لازم يكونوا في الداتا بيز 
-	$employee = "Eman";
-	$password ="12345";
-    session_start();
+session_start();
+	$username="";
+	$password ="";
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		$username =	$_POST['name'];
+		$password = $_POST['pass'];
+}
+// created connectin	
+  $conn = mysqli_connect("localhost","root","","log");
 
+  if($conn){
+  	echo "yes"."<br>";
+  }
+  $sql  = "CREATE TABLE users (username VARCHAR(50) PRIMARY KEY,password INT(50), firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL ,email VARCHAR(50), mobilenumber INT(15) ,sex VARCHAR(50),deparments VARCHAR(50),city VARCHAR(50), country VARCHAR(50),dateOfBirth datetime)";
+
+  if(mysqli_query($conn,$sql)){
+  	echo "Table users created";
+  }
+  else{
+  	echo "Table users not created";
+  }
+
+ $ins ="INSERT INTO users (username,password,firstname,lastname,email,mobilenumber,sex,deparments,city,country,dateOfBirth) VALUES ('EmanHass','20180293','Eman','Hassouna','eman@gmail.com','0596541234','female','information technology','Gaza','Palestine','2000-10-22')";
+$in ="INSERT INTO users (username,password,firstname,lastname,email,mobilenumber,sex,deparments,city,country,dateOfBirth) VALUES ('AsalaElqeeq','20183333','Asala','Elqeeq','Asala@gmail.com','0595749621','female','information technology','Gaza','Palestine','2000-2-2')";
+if(mysqli_query($conn,$ins)){
+	echo "<br>"."New record created successfully";
+}
+else{
+	echo "<br>"."New record Not created";
+}
+if(mysqli_query($conn,$in)){
+	echo "<br>"."New record created successfully";
+}
+else{
+	echo "<br>"."New record Not created";
+}
+$select = "select * from users where username='$username' and password ='$password' ";
+
+$result = mysqli_query($conn,$select);
+
+$row = mysqli_fetch_assoc($result);
+
+if($row){
+	echo "<br>"." row Not null";
+}
+else{
+	echo "<br>"." row null";
+}
+echo "<br>".$row['username'];
+echo "<br>".$row['firstname'];
+echo "<br>".$row['lastname'];
 	if(isset($_POST['sub'])){
-		if($_SERVER['REQUEST_METHOD']=='POST'){
-
-			if($_POST['name']== $employee and $password== $_POST['pass']){
-					if(isset($_POST['remmeber'])){
-					  setcookie('username',$employee,time()+3600,"/");
-					  setcookie('password',$password,time()+3600,"/");
-						  if(isset($_COOKIE['username']) and isset($_COOKIE['password'])){
-						  	$username =$_COOKIE['username'];
-						  	$pass =$_COOKIE['password'];
-						  	echo "<script>
-						  		document.getElementById('pass').value=$pass;
-						  		
-						  	</script>";
-						  }					    
-					}
-				$_SESSION['username']=$_POST['name'];
-			    echo  "<h2>Welcome ".$_POST['name']." We Will transfer to your page</h2>";
-			    header("refresh:1;URL=profile_employee.php");			    
-				
-
-			}
-			else{
-				echo "<div>";
-					echo "<h2>You can't to login</h2>";
-				echo "</div>";
-			}
+		if ($row['username'] == $username  and $row['password'] == $password) {
 			
+			if(isset($_POST['remmeber'])){
+				setcookie('user',$username,time()+(86400 * 30),"/");
+				setcookie('password',$password,time()+(86400 * 30),"/");
+					
+					if(isset($_COOKIE['user']) and isset($_COOKIE['password'])){
+						$user =$_COOKIE['user'];
+						$pass =$_COOKIE['password'];
+						echo "<script>
+						document.getElementById('pass').value=$pass;
+						document.getElementById('name').value=$user;
+									  		
+						</script>";
+					}					    
+			}
+					$_SESSION['username']= $_POST['name'];
+				    echo  "<h2>Welcome ".$_POST['name']." We Will transfer to your page</h2>";
+				    header("refresh:3;URL=profile_employee.php");
+		}
+		else{
+				echo "<div>";
+					echo "<h2>Failed You can't to login</h2>";
+				echo "</div>";			
 		}		
 	}
-
 ?>
